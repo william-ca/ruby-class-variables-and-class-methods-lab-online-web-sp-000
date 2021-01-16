5ae203d7eac9a47465a98a81dd7dde4c8e95ccb6
Will Andre <wandre94@gmail.com>
2021-01-16 16:14:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,9 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-
+  @@artists = []
+  @@genres = []
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
