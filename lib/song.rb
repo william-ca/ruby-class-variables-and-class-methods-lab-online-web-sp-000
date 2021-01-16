ba2cf2a4f9de77a66d84b98f20369a10e7ef90b5
Will Andre <wandre94@gmail.com>
2021-01-16 16:43:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
 
   def self.artist_count
     artist_count = Hash.new(0)
-    @@artists.each {|artist| genre_count[genre] += 1}
+    @@artists.each {|artist| artist_count[artist] += 1}
     artist_count
   end
 
